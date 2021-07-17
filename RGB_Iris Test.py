24f6be0d19fe0172827d9b2c96f9785b214504e5
simon <sbrun068@uottawa.ca>
2021-07-17 15:05:20 -0400

    The old Main has been moved to the p1_identify_regions() and p1_only_details() functions. P1 stand for part 1 of the project: identifying the important regions of the eye. p1_identify_regions() : displays our changes to the image and prints relevant details. p1_only_details() : only prints relevant details to the console (drastically shortens runtime).
    The main only holds the p1_identify_regions() and p1_only_details() functions in comments.
    
    Docstrings have been added to applicable functions. Variable and function names have been refactored
    to better represent their roles and purpose. Added parameter types and return types in functions where applicable.
    
    Print and display functions from the read_image() function have been removed and relocated elsewhere. The printing and displaying has been relocated to the p1_identify_regions() and p1_only_details() main functions.
    
    The isolate_pupil() (and or whiten_region()) function(s) are the only functions left with edge cases that need to be reviewed and improved accordingly.
    Once we can handle all or most of the edge cases, we can look into combining some of the functions we have together before moving on to p2 of the project: finding important iris features.

--- RGB_Iris Test.py
+++ RGB_Iris Test.py
@@ -8,8 +8,6 @@ import cv2 as cv
 def read_image(name: str):
     'Returns a copy of the file'
     image = cv.imread(name)
-    print(name, " - ", image.shape)
-    display(image, 2000, 'Initial Image')
     return image
 
 
@@ -20,8 +18,9 @@ def display(image, time: int = 2000, title: str = 'Image') -> None:
     # cv.destroyWindow(title)
 
 
+# Review
 def isolate_pupil(image, threshold: int):
-    'Binarize value based on if it is below the threshold or not'
+    'Returns a binarized version of the image value based on if it is below a threshold or not'
     x, y, c = image.shape
     x_0 = x // 2
     y_0 = y // 2
@@ -36,16 +35,17 @@ def isolate_pupil(image, threshold: int):
                 image[i, j] = [255, 255, 255]
             else:
                 image[i, j] = [0, 0, 0]
-
     return image
 
 
-def clean_pupil(image, centerX, centerY):
+# Review
+def whiten_region(image, centerX, centerY):
+    "Returns the image after all pixels that are too far from the approximate center point of the pupil are whitened"
     x, y, c = image.shape
-    rand = 43
+    random_range = 43  # number of pixels away from the approximate center point of the pupil. Implement dynamically.
     for i in range(0, x):
         for j in range(0, y):
-            if abs(i - centerX) > rand or abs(j - centerY) > rand:
+            if abs(i - centerX) > random_range or abs(j - centerY) > random_range:
                 image[i, j] = [255, 255, 255]
             else:
                 continue
@@ -53,7 +53,7 @@ def clean_pupil(image, centerX, centerY):
 
 
 def center_mass(image) -> tuple:
-    'Find x, y coordinate of the center of mass'
+    'Returns x, y coordinate of the approximate center of mass of the pupil region'
     sum_i = sum_j = 0
     total = 0
     x, y, c = image.shape
@@ -77,9 +77,10 @@ def add_point(image, point: tuple, size: int):
     return image
 
 
-def find_corner(image) -> tuple:
+def find_corners(image) -> tuple:
+    "Returns the coordinates of the top-left and bottom-right corners of the pupil region"
     x, y, c = image.shape
-    l = list()
+    extremities = list()
 
     counter = 0
     temp1 = (0, 0)
@@ -88,12 +89,12 @@ def find_corner(image) -> tuple:
             r, g, b = image[i, j][2], image[i, j][1], image[i, j][0]
             if r == 0 and g == 0 and b == 0 and counter == 0:  # leftmost point
                 counter += 1
-                l.append(i)
+                extremities.append(i)
             elif r == 0 and g == 0 and b == 0:  # rightmost point
                 temp1 = i
             else:
                 continue
-    l.append(temp1)
+    extremities.append(temp1)
 
     counter = 0
     temp2 = (0, 0)
@@ -102,16 +103,17 @@ def find_corner(image) -> tuple:
             r, g, b = image[i, j][2], image[i, j][1], image[i, j][0]
             if r == 0 and g == 0 and b == 0 and counter == 0:  # topmost point
                 counter += 1
-                l.append(j)
+                extremities.append(j)
             elif r == 0 and g == 0 and b == 0:  # bottommost point
                 temp2 = j
             else:
                 continue
-    l.append(temp2)
-    return (l[0], l[3]), (l[1], l[2])
+    extremities.append(temp2)
+    return (extremities[0], extremities[3]), (extremities[1], extremities[2])
 
 
-def avg_luminance(image) -> int:
+def average_luminance(image) -> int:
+    "Returns the average luminance found across the entire image for thresholding purposes"
     sum = 0
     count = 0
     x, y, c = image.shape
@@ -124,19 +126,21 @@ def avg_luminance(image) -> int:
     return sum // count
 
 
-def fill_pupil(image):
+def fill_pupil(image) -> None:
+    "Fills the area of the pupil reflecting light due to the picture's flash in black"
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(image, contours, 1, (0, 0, 0), thickness=-1)
 
 
-def shape_pupil(image, centerX, centerY, radius):
-    # need to find the radius first
+def circle_region(image, centerX: int, centerY: int, radius: int) -> None:
+    "Draws a circle around a specified area on the picture. Used to identify the pupil and iris regions of the eye"
     cv.circle(image, (centerY, centerX), radius, (0, 255, 0), thickness=2)
 
 
 def recenter_point(bottom_left: tuple, top_right: tuple) -> tuple:
+    "Returns the coordinates of the re-centered center point of the pupil region"
     x1, y1 = bottom_left
     x2, y2 = top_right
     center = (int((x1 + x2) / 2), int((y1 + y2) / 2))
@@ -144,6 +148,7 @@ def recenter_point(bottom_left: tuple, top_right: tuple) -> tuple:
 
 
 def pupil_radius(bottom_left: tuple, top_right: tuple) -> int:
+    "Returns the radius of the pupil region"
     x1, y1 = bottom_left
     x2, y2 = top_right
     radius = int((abs(x2 - x1) / 4 + abs(y2 - y1) / 4))
@@ -151,6 +156,7 @@ def pupil_radius(bottom_left: tuple, top_right: tuple) -> int:
 
 
 def iris_radius(image, pupil_center: tuple, pupil_radius: int, threshold: int) -> int:
+    "Returns the radius of the iris region"
     x = pupil_center[0] - pupil_radius
     edge = False
     while not edge and x != 0:
@@ -161,65 +167,139 @@ def iris_radius(image, pupil_center: tuple, pupil_radius: int, threshold: int) -
     return pupil_center[0] - x + pupil_radius
 
 
+def p1_identify_regions():
+    "Part 1 -  Prints details and outputs the utilized identification process for the pupil and iris regions of an eye"
+    run_program = 'yes'
+
+    while run_program == 'yes':
+        # Read the image
+        print("Which image do you want to process?")
+        name = input().strip()
+        original_image = read_image(name)
+        image = read_image(name)
+
+        # Display the original image and print its properties
+        print('Image Properties: \n', 'Name:', name, "\n", 'Width:', original_image.shape[0], "pixels\n", 'Height:',
+              original_image.shape[1], 'pixels')
+        display(image, 2000, 'Original')
+
+        # Find average luminance - threshold
+        threshold = average_luminance(image)
+        print('Average luminance - threshold value:', threshold, 'RGB')
+
+        # Binarize image based on threshold
+        binarized = isolate_pupil(image, threshold)
+        display(binarized, 2000, 'After Binarization')
+
+        # Median blur to remove noise
+        blurred = cv.medianBlur(image, 5)
+        display(blurred, 2000, 'Blurred')
+
+        # Find approximate center of mass of the pupil region
+        centerX, centerY = center_mass(blurred)
+        print('Pupil region approximate center of mass:', center_mass(blurred))
+
+        # Whiten picture to isolate pupil region
+        whitened = whiten_region(blurred, centerX, centerY)
+        display(whitened, 2000, 'Whitened')
+
+        # Fill the pupil region in black
+        fill_pupil(whitened)
+        display(whitened, 2000, 'Filled')
+
+        # Find top-left and bottom-right corners of the pupil region
+        corners = find_corners(whitened)
+        print('Top-left corner:', corners[0], ' - ', 'Bottom-right corner:', corners[1])
+
+        # Find the exact center of mass of the pupil region
+        center = recenter_point(corners[0], corners[1])
+        print('Exact center of mass:', center)
+
+        # Add the point of the exact center of mass of the pupil region
+        add_point(whitened, center, 5)
+        display(whitened, 2000, 'Center')
+
+        # Find the radius of the pupil region
+        radius = pupil_radius(corners[0], corners[1])
+        print('Pupil radius =', radius, 'pixels')
+
+        # Find radius of the iris region
+        thresh = 255 - threshold
+        iris_rad = iris_radius(image, center, radius, thresh)
+        print("Iris radius =", iris_rad, 'pixels')
+
+        # Circles the pupil region on the original image
+        circle_region(original_image, center[0], center[1], radius)
+        display(original_image, 2000, 'pupil')
+
+        # Circles the iris region on the original image
+        circle_region(original_image, center[0], center[1], iris_rad)
+        display(original_image, 2000, 'iris')
+
+        # Ask user if he wishes to continue and repeat the process
+        print("Do you want to continue the program? (yes or no)")
+        run_program = input().strip()
+
+
+def p1_only_details():
+    "Part 1 - Only prints details. Does not display the region identification process and transformations"
+    run_program = 'yes'
+
+    while run_program == 'yes':
+        # Read the image
+        print("Which image do you want to process?")
+        name = input().strip()
+        original_image = read_image(name)
+        image = read_image(name)
+
+        # Print original image properties and display original
+        print('Image Properties: \n', 'Name:', name, "\n", 'Width:', original_image.shape[0], "pixels\n", 'Height:',
+              original_image.shape[1], 'pixels')
+
+        # Find average luminance - threshold
+        threshold = average_luminance(image)
+        print('Average luminance - threshold value:', threshold, 'RGB')
+
+        # Binarize image based on threshold
+        isolate_pupil(image, threshold)
+
+        # Median blur to remove noise
+        blurred = cv.medianBlur(image, 5)
+
+        # Find approximate center of mass of the pupil region
+        centerX, centerY = center_mass(blurred)
+        print('Pupil region approximate center of mass:', center_mass(blurred))
+
+        # Whiten picture to isolate pupil region
+        whitened = whiten_region(blurred, centerX, centerY)
+
+        # Fill the pupil region in black
+        fill_pupil(whitened)
+
+        # Find top-left and bottom-right corners of the pupil region
+        corners = find_corners(whitened)
+        print('Top-left corner:', corners[0], ' - ', 'Bottom-right corner:', corners[1])
+
+        # Find the exact center of mass of the pupil region
+        center = recenter_point(corners[0], corners[1])
+        print('Exact center of mass:', center)
+
+        # Find the radius of the pupil region
+        radius = pupil_radius(corners[0], corners[1])
+        print('Pupil radius =', radius, 'pixels')
+
+        # Find radius of the iris region
+        thresh = 255 - threshold
+        iris_rad = iris_radius(image, center, radius, thresh)
+        print("Iris radius =", iris_rad, 'pixels')
+
+        # Ask user if he wishes to continue and repeat the process
+        print("Do you want to continue the program? (yes or no)")
+        run_program = input().strip()
+
 ##########################################################
 # MAIN
 ##########################################################
 
-run_program = 'yes'
-
-while run_program == 'yes':
-    # Read Image
-    print("Which image do you want to process?")
-    name = input().strip()
-    image = read_image(name)
-    original = read_image(name)
-
-    # Binarize based on Threshold
-    threshold = avg_luminance(image)
-    new_img = isolate_pupil(image, threshold)
-    display(new_img, 2000, 'After Binarization')
-
-    # Blur to remove noise
-    blurred = cv.medianBlur(image, 5)
-    display(blurred, 2000, 'Blurred')
-
-    # Clean pupil
-    centerX, centerY = center_mass(blurred)
-    cleaned = clean_pupil(blurred, centerX, centerY)
-    display(cleaned, 2000, 'Cleaned')
-    print(center_mass(blurred))
-
-    # Fill pupil
-    fill_pupil(cleaned)
-    display(cleaned, 2000, 'filled')
-
-    # Find corners
-    corners = find_corner(cleaned)
-    print(corners)
-
-    # Recenter point
-    center = recenter_point(corners[0], corners[1])
-    print(center)
-    add_point(cleaned, center, 5)
-    display(cleaned, 2000, 'recentered')
-
-    # Find pupil radius
-    radius = pupil_radius(corners[0], corners[1])
-    print('Pupil rad = ', radius)
-
-    # Find iris radius
-    t = 255 - threshold
-    iris_rad = iris_radius(image, center, radius, t)
-    print("Iris rad = ", iris_rad)
-
-    # Shape pupil
-    shape_pupil(original, center[0], center[1], radius)
-    display(original, 2000, 'pupil')
-
-    # Shape iris
-    shape_pupil(original, center[0], center[1], iris_rad)
-    display(original, 2000, 'iris')
-
-    # Ask user if he wishes to continue
-    print("Do you want to continue the program? (yes or no)")
-    run_program = input().strip()
+# p1_identify_regions()
+# p1_only_details()
